db90e6128242667dc6f901a7e90f047a85450c02
tkssharma <tarun.softengg@gmail.com>
2020-07-26 09:20:40 +0530

    naming updates

--- Day 11 Redux/src/_actions/user.actions.js
+++ Day 11 Redux/src/_actions/user.actions.js
@@ -1,6 +1,6 @@
 import { userConstants } from '../_constants';
 import { userService } from '../_services';
-import { alertActions } from './';
+import { alertActions } from '.';
 import { history } from '../_helpers';
 
 export const userActions = {
